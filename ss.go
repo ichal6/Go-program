79182888ae6ad77c5920249d3ec152fbdb18d26a
Aneta-Dudek <57062491+Aneta-Dudek@users.noreply.github.com>
2020-10-18 12:34:37 +0200

    Make swap shorter

--- ss.go
+++ ss.go
@@ -46,9 +46,7 @@ func bubbleSort(array []int) []int {
     for i := 0; i < arrayLength-1; i++ {
         for j := 0; j < arrayLength-i-1; j++ {
             if array[j] > array[j+1]{
-                temp := array[j]
-                array[j] = array[j+1]
-                array[j+1] = temp
+                array[j], array[j+1] = array[j+1], array[j]
             }
         }
     }
