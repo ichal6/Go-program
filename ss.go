e58ad9e5b8e5ee8b877fa8e054e7bae66d98ed96
Aneta-Dudek <57062491+Aneta-Dudek@users.noreply.github.com>
2020-10-18 11:04:45 +0200

    Update ss.go file

--- ss.go
+++ ss.go
@@ -1,9 +1,15 @@
-package main 
-  
+//Declare a main package (a package is a way to group functions).
+package main
+
+//Import the popular fmt package, which contains functions for formatting text, including printing to the console.
+//This package is one of the standard library packages you got when you installed Go.
 import "fmt"
-  
-// Main function 
-func main() { 
-  
-    fmt.Println("!... Hello World ...!") 
-} 
+//You can use the pkg.go.dev site to find published modules whose packages have functions you can use in your own code.
+import "rsc.io/quote"
+
+//Implement a main function to print a message to the console.
+//A main function executes by default when you run code in the file.
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println(quote.Go())
+}
