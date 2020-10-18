df846d954b6295ccb7bf55870b8ea1dc1fde9721
Aneta-Dudek <57062491+Aneta-Dudek@users.noreply.github.com>
2020-10-18 12:08:02 +0200

    Refactor code and call ssp.go function

--- ss.go
+++ ss.go
@@ -4,22 +4,21 @@ package main
 //Import the popular fmt package, which contains functions for formatting text, including printing to the console.
 //This package is one of the standard library packages you got when you installed Go.
 import "fmt"
-
 //Implement a main function to print a message to the console.
 //A main function executes by default when you run code in the file.
 
-const sizeOfArray int = 10
-
 func main() {
-    arrayOfNumber := [sizeOfArray]int{2 ,6, 9, 8, 20, 15, 3, 5, 6, 9}
-    min, max := findMinAndMax(arrayOfNumber)
-    average := findAverage(arrayOfNumber)
+    arrayOfNumbers := []int{2 ,6, 9, 8, 20, 15, 3, 5, 6, 9}
+    min, max := findMinAndMax(arrayOfNumbers)
+    average := findAverage(arrayOfNumbers)
     fmt.Println("Min = ", min)
     fmt.Println("Max = ", max)
     fmt.Println("Average = ", average)
+
+    testPackages()
 }
 
-func findMinAndMax(a [sizeOfArray]int) (min int, max int) {
+func findMinAndMax(a []int) (min int, max int) {
 	min = a[0]
     max = a[0]
 	for _, value := range a {
@@ -33,7 +32,7 @@ func findMinAndMax(a [sizeOfArray]int) (min int, max int) {
 	return min, max
 }
 
-func findAverage(xs [sizeOfArray]int) int {
+func findAverage(xs []int) int {
     total := int(0)
     for _, x := range xs {
       total += x
